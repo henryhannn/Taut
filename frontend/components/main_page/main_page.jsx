e063bfad44609cafe2e225418027fefd27148e20
henryhannn <henryhan3996@gmail.com>
2020-09-30 10:32:39 -0700

    remove image

--- frontend/components/main_page/main_page.jsx
+++ frontend/components/main_page/main_page.jsx
@@ -12,7 +12,6 @@ class MainPage extends React.Component {
         return (
             <div className="main-page">
                 <Header logout={this.props.logout}/>
-                <img src="https://i.kym-cdn.com/entries/icons/original/000/028/021/work.jpg" srcSet="https://i.kym-cdn.com/entries/icons/original/000/028/021/work.jpg"></img>
                 {/* <ChannelListContainer />
                 <MessageAreaContainer /> */}
             </div>
