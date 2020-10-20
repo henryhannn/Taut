615b1a538ab2146bcaac2d6893f3e82850210215
henryhannn <henryhan3996@gmail.com>
2020-10-20 00:31:31 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:index, :show, :create]
-    resources :channels, only: [:index, :show, :create] do
+    resources :users, only: [:index, :show, :create]
+
+    resources :channels, only: [:index, :show, :create, :destroy]
+    resources :channels do
       resources :messages, only: [:index]
+      resources :memberships, only: [:index]
     end
+
     resource :session, only: [:create, :destroy]
+
     resources :messages, only: [:create, :destroy]
+
+    resources :memberships, only: [:create]
   end
 
   root "static_pages#root"
