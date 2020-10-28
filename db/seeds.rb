822ec8c6bfbf1913a76420d8f5eb7248944843e0
henryhannn <henryhan3996@gmail.com>
2020-10-27 23:42:52 -0700

    add imports to message area

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.delete_all
+Message.delete_all
+Channel.delete_all
+Membership.delete_all
 
 demouser = User.create(email: 'DemoUser@Demo.com', full_name: 'Demo', password: 'demouser')
+user1 = User.create(email: 'DemoUser@Demo.com', full_name: 'Demo', password: 'demouser')
+user2 = User.create(email: 'DemoUser@Demo.com', full_name: 'Demo', password: 'demouser')
+user3 = User.create(email: 'DemoUser@Demo.com', full_name: 'Demo', password: 'demouser')
+user4 = User.create(email: 'DemoUser@Demo.com', full_name: 'Demo', password: 'demouser')
+user5 = User.create(email: 'DemoUser@Demo.com', full_name: 'Demo', password: 'demouser')
+
 channel1 = Channel.create(name: 'general', channel_type: 'group')
 channel2 = Channel.create(name: 'announcements', channel_type: 'group')
 channel3 = Channel.create(name: 'brainstorming', channel_type: 'group')
@@ -21,12 +30,11 @@ channel9 = Channel.create(name: 'meme-team', channel_type: 'group')
 channel10 = Channel.create(name: 'red-velvet-team', channel_type: 'group')
 channel11 = Channel.create(name: 'running-out-of-channel-name-ideas1', channel_type: 'group')
 channel12 = Channel.create(name: 'running-out-of-channel-name-ideas2', channel_type: 'group')
-channel13 = Channel.create(name: 'running-out-of-channel-name-ideas3', channel_type: 'group')
-channel14 = Channel.create(name: 'running-out-of-channel-name-ideas4', channel_type: 'group')
-channel15 = Channel.create(name: 'direct1', channel_type: 'direct')
-channel16 = Channel.create(name: 'direct2', channel_type: 'direct')
-channel17 = Channel.create(name: 'direct3', channel_type: 'direct')
-channel18 = Channel.create(name: 'direct4', channel_type: 'direct')
-channel19 = Channel.create(name: 'direct5', channel_type: 'direct')
-channel20 = Channel.create(name: 'direct6', channel_type: 'direct')
-channel21 = Channel.create(name: 'direct7', channel_type: 'direct')
+
+dm1 = Channel.create(name: 'direct1', channel_type: 'direct')
+dm2 = Channel.create(name: 'direct2', channel_type: 'direct')
+dm3 = Channel.create(name: 'direct3', channel_type: 'direct')
+dm4 = Channel.create(name: 'direct4', channel_type: 'direct')
+dm5 = Channel.create(name: 'direct5', channel_type: 'direct')
+dm6 = Channel.create(name: 'direct6', channel_type: 'direct')
+dm7 = Channel.create(name: 'direct7', channel_type: 'direct')
--- frontend/components/main_page/message_area.jsx
+++ frontend/components/main_page/message_area.jsx
@@ -1,18 +1,13 @@
 // main message area
 
 import React from 'react';
+import MessageContent from './message_content';
+import MessageForm from './message_form';
+import AboutChannelContainer from './about_channel_container';
 
 class MessageArea extends React.Component {
     constructor(props) {
         super(props);
-    }
-
-    componentDidMount() {
-
-    }
-
-    componentDidUpdate(newProps) {
-
     }
 
     render() {
