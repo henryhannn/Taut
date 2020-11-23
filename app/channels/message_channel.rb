a5b54fda591021a3e0cacf204de5bfb7089edb86
henryhannn <henryhan3996@gmail.com>
2020-11-23 03:49:38 -0800

    update message channels

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,5 +1,25 @@
 class MessageChannel < ApplicationCable::Channel
   def subscribed 
-      stream_for 'message_channel'
-  end 
+    stream_for 'message_channel'
+  end
+
+  def speak(data)
+    message = Message.create(
+      body: data['message'],
+      author_id: data['author_id'],
+      channel_id: data['channel_id']
+    )
+
+    socket = {
+      id: message.id,
+      body: message.body,
+      author_id: message.author_id,
+      channel_id: message.channel_id,
+      author: message.author.full_name
+    }
+
+    MessageChannel.broadcast_to('message_channel', socket)
+  end
+
+  def unsubscribed; end
 end
